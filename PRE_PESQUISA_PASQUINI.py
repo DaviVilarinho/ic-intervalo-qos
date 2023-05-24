0eb6cabab757dde4b9dcde0f8da5354e32ee6cc2
DaviVilarinho <davi.r.vilarinho@gmail.com>
2023-05-24 17:09:51 -0300

    feita primeira parte

--- PRE_PESQUISA_PASQUINI.py
+++ PRE_PESQUISA_PASQUINI.py
@@ -1,13 +1,21 @@
-import numpy as np
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+import numpy as np
+from sklearn.metrics import mean_squared_error
 
 BASE_DIR = "../"
 PASQUINIS_PATH = BASE_DIR + "traces-netsoft-2017"
-RESULTS_VIDEO_PATH = "./resultados_pre_pesquisa/resultado_video.txt"
+RESULTS_VIDEO_PATH = "./resultados_pre_pesquisa/resultado_linhas_video.txt"
+RESULTS_AUDIO_PATH = "./resultados_pre_pesquisa/resultado_linhas_audio.txt"
+INTERVAL_VIDEO_PATH = "./resultados_pre_pesquisa/resultado_intervalos_video.txt"
+INTERVAL_AUDIO_PATH = "./resultados_pre_pesquisa/resultado_intervalos_audio.txt"
 
 TRACES=[
     #"KV-BothApps-FlashcrowdLoad",
@@ -19,61 +27,81 @@ TRACES=[
     #"VoD-SingleApp-FlashcrowdLoad",
     "VoD-SingleApp-PeriodicLoad"]
 
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import os
+def start_results_file(path):
+    with open(path, 'w') as logger:
+        logger.write(f'quantidade_linhas, predict_random_forest, regression_tree\n')
 
-#NROWS = 7e3
-def read_traces(traces, NROWS):
+def get_nmae_and_predict_random_and_regression(X, Y) -> tuple:
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.7, random_state=42)
+    
+    regression_tree = DecisionTreeRegressor(max_depth=2) # a classification or regression decision tree is used as a predictive model to draw conclusions about a set of observations. 
+    regression_tree.fit(X_train, y_train)
+
+    regr_random_forest = RandomForestRegressor(n_estimators=120, random_state=0)
+    regr_random_forest.fit(X_train, y_train)
+
+    y_random_forest = regr_random_forest.predict(X_test)
+    y_reg_tree = regression_tree.predict(X_test)
+    
+    return (mean_squared_error(y_test, y_random_forest), mean_squared_error(y_test, y_reg_tree))
+
+def save_results(path, key=None, random_forest=None, regression_tree=None):
+    with open(path, 'a+') as logger:
+        logger.write(f'{key}, {random_forest}, {regression_tree}\n')
+
+def read_traces(traces, NROWS=None):
     csvs = {}
 
     for root, subfolders, files in os.walk(traces):
         for file in files:
             if file.find("csv") >= 0:
-                csvs[file] = pd.read_csv(f'{root}/{file}', nrows=NROWS)
+                csvs[file] = pd.read_csv(f'{root}/{file}', nrows=28500)
 
     return csvs
 
-for NROWS in [1e3, 2e3, 4e3, 8e3, 16e3, 28950]:
-    VOD_SINGLEAPP_PERIODIC_LOAD = read_traces(f'{PASQUINIS_PATH}/VoD-SingleApp-PeriodicLoad', NROWS)
-
-    print(VOD_SINGLEAPP_PERIODIC_LOAD.keys())
-
-    def cria_x_y(csvs_map: dict):
-        return (pd.concat([csvs_map["X_flow.csv"], csvs_map["X_cluster.csv"], csvs_map["X_port.csv"]], axis=1).apply(pd.to_numeric, errors='coerce').fillna(0),
-                csvs_map["Y.csv"].apply(pd.to_numeric, errors='coerce').fillna(0))
-
-    X, y = cria_x_y(VOD_SINGLEAPP_PERIODIC_LOAD)
-
-    y_video = y['DispFrames']
-
-    np.set_printoptions(threshold=sys.maxsize)
-
-    X_video_train, X_video_test, y_video_train, y_video_test = train_test_split(X, y_video, test_size=0.7, random_state=42)
-
-    regr_1 = DecisionTreeRegressor(max_depth=2) # a classification or regression decision tree is used as a predictive model to draw conclusions about a set of observations. 
-    regr_1.fit(X_video_train, y_video_train)
-
-    regr_random_forest = RandomForestRegressor(n_estimators=120, random_state=0)
-    regr_random_forest.fit(X_video_train, y_video_train)
-
-#y_1 = regr_1.predict(X_video_test)
-    y_random_forest = regr_random_forest.predict(X_video_test)
-    y_reg_tree = regr_1.predict(X_video_test)
-
-    from sklearn.metrics import mean_squared_error
-#print(mean_squared_error(y_video_test, y_1))
-    with open(RESULTS_VIDEO_PATH, 'a+') as logger:
-        logger.write(f'{NROWS}, {mean_squared_error(y_video_test, y_random_forest)}, {mean_squared_error(y_video_test, y_reg_tree)}\n')
-    print(mean_squared_error(y_video_test, y_random_forest))
+def cria_x_y(csvs_map: dict):
+    return (pd.concat([csvs_map["X_flow.csv"], csvs_map["X_cluster.csv"], csvs_map["X_port.csv"]], axis=1).apply(pd.to_numeric, errors='coerce').fillna(0),
+            csvs_map["Y.csv"].apply(pd.to_numeric, errors='coerce').fillna(0))
 
 
-# e para áudio
-#y_audio = y#[''] # o que considerar?
-#X_audio_train, X_audio_test, y_audio_train, y_audio_test = train_test_split(X, y_audio, test_size=0.33, random_state=42)
+def main_intervals():
+    start_results_file(INTERVAL_VIDEO_PATH)
+    start_results_file(INTERVAL_AUDIO_PATH)
 
-#regr_random_forest.fit(X, y_audio)d
-#y_audio_random_forest = regr_random_forest.predict(X_test)
+    for interval in [256,128,64,32,16,8,4,2,1]:
+        print(f'Analisando intervalo {interval}')
+        VOD_SINGLEAPP_PERIODIC_LOAD = read_traces(f'{PASQUINIS_PATH}/VoD-SingleApp-PeriodicLoad')
 
-#print(mean_squared_error(y_audio, y_audio_random_forest))
+        print(f'Cria x_y')
+        X, y = cria_x_y(VOD_SINGLEAPP_PERIODIC_LOAD)
+        
+        print(f'Query para {interval}')
+        X.query(f'TimeStamp % {interval} == 0', inplace=True)
+        y.query(f'TimeStamp % {interval} == 0', inplace=True)
+
+        print(f'Prevendo para vídeo com intervalo {interval}')
+        random_forest_nmae, regression_tree_nmae = get_nmae_and_predict_random_and_regression(X, y['DispFrames'])
+        print(f'Salvando para vídeo com intervalo {interval}')
+        save_results(INTERVAL_VIDEO_PATH, key=interval, random_forest=random_forest_nmae, regression_tree=regression_tree_nmae)
+        print(f'Prevendo para áudio com intervalo {interval}')
+        random_forest_nmae, regression_tree_nmae = get_nmae_and_predict_random_and_regression(X, y['noAudioPlayed'])
+        print(f'Salvando para áudio com intervalo {interval}')
+        save_results(INTERVAL_AUDIO_PATH, key=interval, random_forest=random_forest_nmae, regression_tree=regression_tree_nmae)
+         
+def main_different_sizes():
+    start_results_file(RESULTS_AUDIO_PATH)
+    start_results_file(RESULTS_VIDEO_PATH)
+
+    for NROWS in [1e3, 2e3, 4e3, 8e3, 16e3, 28950]:
+        VOD_SINGLEAPP_PERIODIC_LOAD = read_traces(f'{PASQUINIS_PATH}/VoD-SingleApp-PeriodicLoad', NROWS)
+
+        X, y = cria_x_y(VOD_SINGLEAPP_PERIODIC_LOAD)
+
+        random_forest_nmae, regression_tree_nmae = get_nmae_and_predict_random_and_regression(X, y['DispFrames'])
+        save_results(RESULTS_VIDEO_PATH, key=NROWS, random_forest=random_forest_nmae, regression_tree=regression_tree_nmae)
+        random_forest_nmae, regression_tree_nmae = get_nmae_and_predict_random_and_regression(X, y['noAudioPlayed'])
+        save_results(RESULTS_AUDIO_PATH, key=NROWS, random_forest=random_forest_nmae, regression_tree=regression_tree_nmae)
+
+if __name__ == "__main__":
+    #main_different_sizes()
+    main_intervals()
