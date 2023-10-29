1a55349dc59d6d64ada8904a42d12c1a09f7ee28
DaviVilarinho <davi.r.vilarinho@gmail.com>
2023-10-29 08:51:11 -0300

    fix bug

--- experiment_on_univariate.py
+++ experiment_on_univariate.py
@@ -41,7 +41,7 @@ traces = {
     "KV-SingleApp-PeriodicLoad"]
 }
 
-NROWS = 10 if IS_LOCAL else None
+NROWS = 8 if IS_LOCAL else None
 
 def nmae(y_pred, y_test):
     return abs(y_pred - y_test).mean() / y_test.mean()
@@ -97,7 +97,7 @@ for trace_family, traces in traces.items():
                 
                 best_k.append(list(minimal_dataset.columns))
 
-                x_minimal_train, x_minimal_test, y_minimal_train, y_minimal_test = train_test_split(minimal_dataset, y_dataframe, test_size=0.7, random_state=42)
+                x_minimal_train, x_minimal_test, y_minimal_train, y_minimal_test = train_test_split(minimal_dataset, y_dataframe, test_size=0.3, random_state=42)
 
                 regression_tree = DecisionTreeRegressor() 
 
@@ -107,7 +107,7 @@ for trace_family, traces in traces.items():
 
                 pred_reg_tree_minimal = regression_tree.predict(x_minimal_test)
                 
-                tables[y_metric] = pd.concat([tables[y_metric], new_minimal_prediction(k, trace_split[1],trace_split[2], "RegTree", y_metric , nmae(pred_reg_tree_minimal, y_minimal_test[y_metric]), training_minimal_reg_tree_time)], ignore_index=True)
+                tables[y_metric] = pd.concat([tables[y_metric], new_minimal_prediction(k, trace_split[2], trace_split[1], "RegTree", y_metric , nmae(pred_reg_tree_minimal, y_minimal_test[y_metric]), training_minimal_reg_tree_time)], ignore_index=True)
 
                 random_forest_regressor = RandomForestRegressor(n_estimators=120, random_state=42, n_jobs=-1)
 
@@ -117,9 +117,34 @@ for trace_family, traces in traces.items():
 
                 pred_random_forest_minimal = random_forest_regressor.predict(x_minimal_test)
 
-                tables[y_metric] = pd.concat([tables[y_metric],new_minimal_prediction(k, trace_split[1],trace_split[2], "RandomForest", y_metric , nmae(pred_random_forest_minimal, y_minimal_test[y_metric]), random_forest_minimal_training_time)], ignore_index=True)
+                tables[y_metric] = pd.concat([tables[y_metric],new_minimal_prediction(k, trace_split[2],trace_split[1], "RandomForest", y_metric , nmae(pred_random_forest_minimal, y_minimal_test[y_metric]), random_forest_minimal_training_time)], ignore_index=True)
 
 table_iv = pd.merge(tables["ReadsAvg"], tables["DispFrames"], on=["k_best", "LoadPattern", "ExpType", "RegressionMethod"]).sort_values(by=['LoadPattern', 'ExpType', 'k_best'])
+
+def make_original_table_result(load_pattern, exptype, regression_method, video_frame_rate_nmae, video_frame_rate_training_time, read_resp_nmae, read_resp_training_time):
+    return {'LoadPattern': load_pattern,
+            'ExpType': exptype,
+            'RegressionMethod': regression_method,
+            'ReadsAvgNmae': read_resp_nmae,
+            'ReadsAvgTrainingTime': read_resp_training_time,
+            'DispFramesNmae': video_frame_rate_nmae,
+            'DispFramesTrainingTime': video_frame_rate_training_time}
+    
+original_minimal_results = pd.DataFrame([
+    make_original_table_result('PeriodicLoad', 'SingleApp', 'RegTree', 0.14, 1, .02, 1),
+    make_original_table_result('PeriodicLoad', 'BothApps', 'RandomForest', 0.11, 40, .02, 32),
+    make_original_table_result('PeriodicLoad', 'SingleApp', 'RegTree', 0.12, 1, .02, 1),
+    make_original_table_result('PeriodicLoad', 'BothApps', 'RandomForest', 0.11, 38, .02, 30),
+    make_original_table_result('FlashCrowdLoad', 'SingleApp', 'RegTree', 0.11, 1, .02, 1),
+    make_original_table_result('FlashCrowdLoad', 'BothApps', 'RandomForest', 0.09, 37, .02, 16),
+    make_original_table_result('FlashCrowdLoad', 'SingleApp', 'RegTree', 0.10, 1, .02, 1),
+    make_original_table_result('FlashCrowdLoad', 'BothApps', 'RandomForest', 0.06, 24, .02, 11),
+])
+
+table_iv = pd.merge(table_iv, original_minimal_results, on=["LoadPattern", "ExpType", "RegressionMethod"], suffixes=['', 'Original'])
+table_iv['DeltaToOriginalReadsAvgNmae'] = table_iv['ReadsAvgNmae'] - table_iv['ReadsAvgNmaeOriginal']
+table_iv['DeltaToOriginalDispFramesNmae'] = table_iv['DispFramesNmae'] - table_iv['DispFramesNmaeOriginal']
+
 table_iv.to_csv(f'{BASE_RESULTS_PATH}/table_iv.csv')
 with open(f'{BASE_RESULTS_PATH}/best_k.csv', 'w') as best_k_file:
     for each_k in best_k:
