1750269d0c8697618bbc0b86609fc740ee6339ce
DaviVilarinho <davi.r.vilarinho@gmail.com>
2024-02-05 00:12:55 -0300

    criada geração de CDF

--- histogramas_cdfs.py
+++ histogramas_cdfs.py
@@ -43,14 +43,16 @@ for paths in [results_path]:
     except FileExistsError:
         pass
 
+
 def filter_periodic(x, y, period: int):
     x = x[x.index % period == 0]
     y = y[y.index % period == 0]
 
     return x, y
 
-PERIODS = [1, # original
-           2,4,8,16,32,64,128,256]
+
+PERIODS = [1,  # original
+           2, 4, 8, 16, 32, 64, 128, 256]
 
 
 for trace_family, traces in traces.items():
@@ -63,18 +65,38 @@ for trace_family, traces in traces.items():
             # smallest first
             # per switch
 
-            x_trace, y_dataset = parse_traces(trace, y_metric, ['X_cluster.csv', 'X_flow.csv', 'X_port.csv'], nrows=None)
-
+            x_trace, y_dataset = parse_traces(
+                trace, y_metric, ['X_cluster.csv', 'X_flow.csv', 'X_port.csv'], nrows=None)
 
             for period in PERIODS:
-                x_filtered, y_filtered = filter_periodic(x_trace, y_dataset, period)
+                x_filtered, y_filtered = filter_periodic(
+                    x_trace, y_dataset, period)
 
-                counts, bins = np.histogram(y_dataset)
-                plt.stairs(counts, bins)
-                plt.savefig(f'{BASE_RESULTS_PATH}/{period}_{trace}_{y_metric}_dataset_hist.png')
+                hist, bins = np.histogram(
+                    y_filtered, bins=30 if y_metric == 'DispFrames' else 20)
+                hist = hist / np.sum(hist) * 100
 
-                counts, bins = np.histogram(y_dataset, density=True)
-                plt.stairs(counts, bins)
-                plt.savefig(f'{BASE_RESULTS_PATH}/{period}_{trace}_{y_metric}_dataset_cdf.png')
+                plt.figure(figsize=(12, 6))
+                plt.subplot(1, 2, 1)
+                plt.bar(bins[:-1], hist, width=(bins[1]-bins[0]))
+                plt.title('Histograma')
+                plt.ylabel('Porcentagem das Amostras (%)')
+                plt.xlabel(y_metric)
+                plt.ylim(0, 100)
+                plt.xlim(0, 30 if y_metric ==
+                         'DispFrames' else None)
 
+                cdf = np.cumsum(hist)
 
+                plt.subplot(1, 2, 2)
+                plt.plot(bins[:-1], cdf)
+                plt.title('CDF')
+                plt.ylabel('Porcentagem das Amostras (%)')
+                plt.xlabel(y_metric)
+                plt.ylim(0, 100)
+                plt.xlim(0, 30 if y_metric ==
+                         'DispFrames' else None)
+
+                plt.savefig(
+                    f'{BASE_RESULTS_PATH}/{period}_{trace}_{y_metric}_dataset.png')
+                print(f'{period}_{trace}_{y_metric}_dataset')
