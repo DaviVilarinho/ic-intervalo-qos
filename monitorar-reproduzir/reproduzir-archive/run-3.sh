5acd3376755d7bb336441109b8ce89e369d0897b
DaviVilarinho <davi.r.vilarinho@gmail.com>
2024-10-11 07:47:04 -0300

    fix until get merged

--- monitorar-reproduzir/reproduzir-archive/run-3.sh
+++ monitorar-reproduzir/reproduzir-archive/run-3.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 #VIDEO="https://archive.org/details/dr-strangelove-or-how-i-learned-to-stop-worrying-and-love-the-bomb"
+#TIME_INIT="23:00"
+#TIME_END="24:00"
 VIDEO="https://www.youtube.com/watch?v=9-Jl0dxWQs8&pp=ygUEM2IxYg%3D%3D"
+TIME_INIT="13:00"
+TIME_END="14:00"
 
-mpv $VIDEO --start=23:00 --end=24:00 --keep-open=no
+mpv $VIDEO --start=$TIME_INIT --end=$TIME_END --keep-open=no
 
-mpv $VIDEO --start=23:00 --end=24:00 --keep-open=no &
+mpv $VIDEO --start=$TIME_INIT --end=$TIME_END --keep-open=no &
 
 sar -u -B -n DEV 1 90 -o sar_stats-3.data
 
-sadf -d sar_stats-3.data -- -u -B -n DEV | awk -F';' '($3 == "idle" || $2 == "pgfree" || ($3 == "eth1" && $4 == "rxkB/s"))' > sar_stats-3.csv
+sadf -d sar_stats-3.data -- -u -B -n DEV > sar_stats-3.csv
