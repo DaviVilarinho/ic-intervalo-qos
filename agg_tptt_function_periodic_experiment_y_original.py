73257cfa43bad7c6f1b39fe1c5f9e0db4803bf94
DaviVilarinho <davi.r.vilarinho@gmail.com>
2024-12-04 18:23:06 -0300

    tptt done

--- agg_tptt_function_periodic_experiment_y_original.py
+++ agg_tptt_function_periodic_experiment_y_original.py
@@ -17,7 +17,7 @@ warnings.filterwarnings('ignore')
 
 IS_LOCAL = False # os.uname()[1].split('-').pop(0) == "ST"
 RANDOM_STATE = 42
-EXPERIMENT = "agg_function_periodic_experiment_tptt"
+EXPERIMENT = "agg_function_periodic_experiment_y_original_tptt"
 
 PASQUINIS_PATH = "../traces-netsoft-2017"
 DATE = datetime.now().isoformat(timespec='seconds')
@@ -53,33 +53,6 @@ for paths in [results_path]:
 y_metrics = {
     "VOD": ['DispFrames',]# 'noAudioPlayed'],
 }
-
-SWITCH_PORTS = {
-    "SWC1": [0, 1, 2, 3, 4],
-    "SWC2": [5, 6, 7, 8, 9],
-    "SWC3": [10, 11, 12, 13, 14],
-    "SWC4": [15, 16, 17, 18, 19],
-    "SWA1": [20, 21],
-    "SWA2": [22, 23],
-    "SWA3": [24, 25, 26],
-    "SWA4": [27, 28],
-    "SWA5": [29, 30],
-    "SWA6": [31, 32, 33],
-    "SWB1": [34, 35, 36],
-    "SWB2": [37, 38],
-    "SWB3": [39, 40, 41],
-    "SWB4": [42, 43]
-}
-
-switch_from_port = {f'{port}': switch for switch,
-                    ports in SWITCH_PORTS.items() for port in ports}
-
-per_switch_file = f'{BASE_RESULTS_PATH}/per_switch.csv'
-
-with open(per_switch_file, 'w') as f:
-    f.write(
-        f'período,função,carga,apps,feature,método,switch,nmae,\n')
-
 
 def get_per_file_name(per_file_csv):
     return f'{BASE_RESULTS_PATH}/per_{per_file_csv.split(".")[0]}.csv'
@@ -174,33 +147,33 @@ for trace_family, traces in traces.items():
 
 
                     # minimal
-                    k = 12
-                    best_k = []
-                    selectK = SelectKBest(f_regression, k=k)
+                    # k = 12
+                    # best_k = []
+                    # selectK = SelectKBest(f_regression, k=k)
 
-                    selectK.set_output(transform="pandas")
+                    # selectK.set_output(transform="pandas")
 
-                    minimal_dataset = selectK.fit_transform(x_train, y_train)
-                    best_k.append(list(minimal_dataset.columns))
+                    # minimal_dataset = selectK.fit_transform(x_train, y_train)
+                    # best_k.append(list(minimal_dataset.columns))
 
-                    x_train, _, y_train, _ = train_test_split(
-                        minimal_dataset, y_train, test_size=TEST_SIZE, random_state=RANDOM_STATE)
+                    # x_train, _, y_train, _ = train_test_split(
+                    #     minimal_dataset, y_train, test_size=TEST_SIZE, random_state=RANDOM_STATE)
 
-                    regression_tree_regressor = DecisionTreeRegressor()
-                    regression_tree_regressor.fit(x_train, y_train)
+                    # regression_tree_regressor = DecisionTreeRegressor()
+                    # regression_tree_regressor.fit(x_train, y_train)
 
-                    random_forest_regressor = RandomForestRegressor(
-                        n_estimators=RANDOM_FOREST_TREES, random_state=RANDOM_STATE, n_jobs=-1)
-                    random_forest_regressor.fit(x_train, y_train)
+                    # random_forest_regressor = RandomForestRegressor(
+                    #     n_estimators=RANDOM_FOREST_TREES, random_state=RANDOM_STATE, n_jobs=-1)
+                    # random_forest_regressor.fit(x_train, y_train)
 
 
-                    with open(MINIMAL_PATH, 'a') as f:
-                        f.write(
-                            f'{period},{name},{trace_load},{trace_apps},{y_metric},RT,{nmae(regression_tree_regressor.predict(x_test), y_test[y_metric])},\n')
-                        f.write(
-                            f'{period},{name},{trace_load},{trace_apps},{y_metric},RF,{nmae(random_forest_regressor.predict(x_test), y_test[y_metric])},\n')
+                    # with open(MINIMAL_PATH, 'a') as f:
+                    #     f.write(
+                    #         f'{period},{name},{trace_load},{trace_apps},{y_metric},RT,{nmae(regression_tree_regressor.predict(x_test), y_test[y_metric])},\n')
+                    #     f.write(
+                    #         f'{period},{name},{trace_load},{trace_apps},{y_metric},RF,{nmae(random_forest_regressor.predict(x_test), y_test[y_metric])},\n')
 
-                    print(f'univariate {period},{name},{trace_load},{trace_apps},{y_metric},RF')
+                    # print(f'univariate {period},{name},{trace_load},{trace_apps},{y_metric},RF')
 
-                    with open(BEST_K_PATH, 'a') as f:
-                        f.write(f'{period},{name},{best_k},\n')
+                    # with open(BEST_K_PATH, 'a') as f:
+                    #     f.write(f'{period},{name},{best_k},\n')
